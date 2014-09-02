0f7c86c0c1a2c2306c47fe178f9dc7bd3da38ad5
Patrick Huber <prhuber@gmail.com>
2014-09-02 10:01:58 -0400

    initial revision

---
+++ Canonical.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Canonical.WebApp.MvcApplication" Language="C#" %>
---
+++ Canonical.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Canonical.WebApp.Startup))]
+namespace Canonical.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
