9db1e4b4e25e05490eeefda77626cc9999b229a2
Patrick Huber <prhuber@gmail.com>
2014-10-06 08:35:40 -0400

    adds customer controller

---
+++ Hosts/Canonical.DomainServices/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using Canonical.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Canonical.DomainServices.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        // GET api/<controller>
+        public IHttpActionResult Get()
+        {
+            return Ok(new Customer[] { });
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST api/<controller>
+        public IHttpActionResult Post([FromBody]Customer customer)
+        {
+            customer.Id = new Random().Next();
+            return Created<Customer>(
+                string.Format("{0}{1}", Request.RequestUri, customer.Id.ToString()), 
+                customer);
+        }
+
+        // PUT api/<controller>/5
+        public IHttpActionResult Put(int id, [FromBody]string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/<controller>/5
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
